fa4571285a67584f36afb1c2e288f9a5fe7ca251
kasatomorning <kasatomo03@gmail.com>
2023-02-26 18:41:21 +0900

    エラーや環境変数からの読み込みに対応

--- exporter.py
+++ exporter.py
@@ -3,44 +3,74 @@ import json
 from prometheus_client import Gauge, start_http_server
 import threading
 from time import sleep
-
-kasazumi_puuid = "871615b0-686a-58b6-8cb5-8ccfd97ca98a"
+from dotenv import dotenv_values
 
 
 class regularlyGetInfo(threading.Thread):
     def __init__(self):
         super(regularlyGetInfo, self).__init__()
         self.event = threading.Event()
-        self.kasazumi = player(kasazumi_puuid, "kasazumi")
-        # 他のプレイヤーも同様に追加
+        config = dotenv_values(".env")
+        self.players = player(**config)
 
     def run(self):
         while True:
-            self.kasazumi.getInfo()
+            try:
+                self.players.getInfo()
+            except Exception as e:
+                print(e)
+
             sleep(300)
 
 
 class player:
-    def __init__(self, puuid, playername):
-        self.puuid = puuid
-        self.playername = playername
-        self.gauge = Gauge(self.playername + "_rating", self.playername + "'s rating")
+    g = Gauge(
+        "valo_rating",
+        "",
+        ["playername"],
+    )
+
+    def __init__(self, **kwargs):
+        self.dict = kwargs
 
     def getInfo(self):
-        rating = 0
-        MMRDataResponseRaw = requests.get(
-            "https://api.henrikdev.xyz/valorant/v2/by-puuid/mmr/ap/" + self.puuid
-        )
-        if "json" not in MMRDataResponseRaw.headers.get("content-type"):
-            print("Response is not content-type:json")
-            return
-        MMRDataResponse = json.loads(MMRDataResponseRaw.text)
-        if MMRDataResponse["status"] != 200:
-            print("Error occurer while getting a request")
-        else:
+        for playername, puuid in self.dict.items():
+            rating = 0
+            MMRDataResponseRaw = requests.get(
+                "https://api.henrikdev.xyz/valorant/v2/by-puuid/mmr/ap/" + puuid,
+                timeout=(3.0),
+            )
+
+            if "json" not in MMRDataResponseRaw.headers.get("content-type"):
+                print(
+                    "response header content-type is %s",
+                    MMRDataResponseRaw.headers.get("content-type"),
+                )
+                raise ContentTypeMismatchError(
+                    "response header content-type is not json"
+                )
+
+            if MMRDataResponseRaw.status_code != 200:
+                MMRDataResponseRaw.raise_for_status()
+
+            MMRDataResponse = json.loads(MMRDataResponseRaw.text)
+            if MMRDataResponse["status"] != 200:
+                print("request json '.status' is %s", MMRDataResponse["status"])
+                raise JsonBodyNotSuccessfulError("response json '.status' is not 200")
+
             rating = MMRDataResponse["data"]["current_data"]["elo"]
-            self.gauge.set(rating)
-            print(self.playername + "'s rating is " + str(rating))
+            self.g.labels(playername).set(rating)
+            print(playername + "'s rating is " + str(rating))
+
+
+class JsonBodyNotSuccessfulError(Exception):
+    def __init__(self, message) -> None:
+        super().__init__(message)
+
+
+class ContentTypeMismatchError(Exception):
+    def __init__(self, message) -> None:
+        super().__init__(message)
 
 
 if __name__ == "__main__":
